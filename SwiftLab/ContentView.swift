670fdf288e47d71898636961a16790b121f25f84
Guillaume Chauveau <guillaumejchauveau.ops.gecu@outlook.com>
2021-01-20 11:13:33 +0100

    Initial

---
+++ SwiftLab/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftLab
+//
+//  Created by Guillaume Chauveau on 20/01/2021.
+//
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
