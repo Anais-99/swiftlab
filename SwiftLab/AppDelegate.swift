639e54f5d6781aba2b38899556b0d8ee5cea08f6
Guillaume Chauveau <guillaumejchauveau.ops.gecu@outlook.com>
2021-01-22 11:59:07 +0100

    Update AppDelegate.swift (#3)

--- SwiftLab/AppDelegate.swift
+++ SwiftLab/AppDelegate.swift
@@ -13,6 +13,8 @@ import SwiftUI
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
     var window: NSWindow!
+    
+    
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Create the SwiftUI view that provides the window contents.
